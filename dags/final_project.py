f07d9ed878b377d73e2be2ac4c95fcff88367f09
Jameson Bontuyan <jabontz09@gmail.com>
2025-08-01 14:34:18 -0400

    Update final_project.py

--- dags/final_project.py
+++ dags/final_project.py
@@ -13,6 +13,10 @@ from helpers import SqlQueries
 default_args = {
     'owner': 'udacity',
     'start_date': pendulum.now(),
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=3),
+    'catchup': False
 }
 
 @dag(
@@ -28,27 +32,27 @@ def final_project():
 
     start_operator = DummyOperator(task_id='Begin_execution')
 
-    # stage_events_to_redshift = StageToRedshiftOperator(
-    #     task_id='Stage_events',
-    #     table = 'staging_events',
-    #     redshift_conn_id = 'redshift',
-    #     aws_user = aws_connection.login,
-    #     aws_pw = aws_connection.password,
-    #     s3_path = f"s3://{bucket}/log_data",
-    #     region = s3_region,
-    #     json = f"s3://{bucket}/log_json_path.json"
-    # )
+    stage_events_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_events',
+        table = 'staging_events',
+        redshift_conn_id = 'redshift',
+        aws_user = aws_connection.login,
+        aws_pw = aws_connection.password,
+        s3_path = f"s3://{bucket}/log_data",
+        region = s3_region,
+        json = f"s3://{bucket}/log_json_path.json"
+    )
 
-    # stage_songs_to_redshift = StageToRedshiftOperator(
-    #     task_id='Stage_songs',
-    #     table = 'staging_songs',
-    #     redshift_conn_id = 'redshift',
-    #     aws_user = aws_connection.login,
-    #     aws_pw = aws_connection.password,
-    #     s3_path = f"s3://{bucket}/song-data",
-    #     region = s3_region,
-    #     json = "auto"
-    # )
+    stage_songs_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_songs',
+        table = 'staging_songs',
+        redshift_conn_id = 'redshift',
+        aws_user = aws_connection.login,
+        aws_pw = aws_connection.password,
+        s3_path = f"s3://{bucket}/song-data",
+        region = s3_region,
+        json = "auto"
+    )
 
     load_songplays_table = LoadFactOperator(
         task_id='Load_songplays_fact_table',
@@ -99,8 +103,7 @@ def final_project():
 
     stop_operator = DummyOperator(task_id='Stop_execution')
 
-    # start_operator >> [stage_events_to_redshift, stage_songs_to_redshift] >> stop_operator
+    start_operator >> [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table >> [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks >> stop_operator
     
-    start_operator >> load_songplays_table >> [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks >> stop_operator
 
 final_project_dag = final_project()
